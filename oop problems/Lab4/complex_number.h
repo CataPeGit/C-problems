1ba0ff31dadf5eb32bb3fdf31a1fd78ed2dccffa
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-03-20 21:56:17 +0200

    Update complex_number.h

--- oop problems/Lab4/complex_number.h
+++ oop problems/Lab4/complex_number.h
@@ -2,25 +2,43 @@
 #include <string>
 #include <fstream>
 
-class Complex{
+class Complex {
 
 public:
     Complex();
     Complex(float real, float imag);
-    
+
     float getReal() const;
     float getImag() const;
-    
-    float setReal(float real);
-    float setReal(float i);
-    
-    Complex conjugate();
-    
-    Complex operator+(const Complex& b) const;
-    
-    
+
+    void setReal(float real);
+    void setImag(float i);
+
+    Complex conjugate() const;
+    Complex add(Complex a) const;
+    Complex subtract(Complex c) const;
+    Complex multiply(Complex c) const;
+
+    void multiply(float a);
+
+    bool equals(Complex other) const;
+
+    float magnitude();
+    float phase();
+    void toPolar(float* r, float* theta);
+
+    std::string toString() const;
+
+    Complex operator-(const Complex& c) const;
+    Complex operator*(const Complex& c) const;
+
+    friend Complex operator+(const Complex& c1, const Complex& c2);
+    friend std::istream& operator>>(std::istream& in, Complex& c);
+    friend std::ostream& operator<<(std::ostream& out, const Complex& c);
+
+
 private:
     float real;
     float imag;
 
-}
+};
