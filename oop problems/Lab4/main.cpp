d8f8771d51e4454232d6954cf33cd4a24f1b714f
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-03-20 21:57:07 +0200

    Update main.cpp

--- oop problems/Lab4/main.cpp
+++ oop problems/Lab4/main.cpp
@@ -1,15 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <iostream>
 #include "complex_test.h"
 
-int main(int argc, char** argv) {
 
+int main(int argc, char** argv) {
+	
 #if ENABLE_TESTS > 0
 	run_complex_tests(true);
 #endif
 	// TODO your code here
+
+	Complex c{ 4, -3 };
+	Complex c2{ 1, 1 };
+	Complex c2_equal{ 1, 1 };
+	//cout <<
+	c.getReal();
+	c.getImag();
+	c.setImag(3);
+	c.setReal(-4);
+
+	c.conjugate();
+	c.add(c2);
+	c.subtract(c2);
+	c.multiply(c2);
+
+	float f = 3;
+	c.multiply(f);
+
+	c2.equals(c2_equal);
+
+	c.magnitude();
+	c.phase();
+	float r, y;
+	c.toPolar(&r, &y);
+
+	c.toString();
 	
 	return 0;
 }
