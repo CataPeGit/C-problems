c00cad882bfe93ba2afc3c1b564c7c3b526edc8f
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-03-26 20:54:10 +0200

    added overloader header

--- oop problems/Lab extra 2/Image.h
+++ oop problems/Lab extra 2/Image.h
@@ -16,39 +16,16 @@ public:
 	unsigned int height() const;
 	
 	bool load(std::string imagePath);
-	//bool save(std::string imagePath);
+	bool save(std::string imagePath);
 
 	Size size() const;
 
-	Image& operator=(const Image& other);
-/*
-	Image operator+(const Image& i);
-	Image operator-(const Image& i);
-	Image operator*(const Image& i);
-
-	bool getROI(Image& roiImg, Rectangle roiRect);
-	bool getROI(Image& roiImg, unsigned int x, unsigned int y, unsigned int width, unsigned int height);
-
-	bool isEmpty() const;
-
-
-
-	unsigned char& at(unsigned int x, unsigned int y);
-	unsigned char& at(Point pt);
-
-	unsigned char* row(int y);
-
-	void release();
 
 	friend std::ostream& operator<<(std::ostream& os, const Image& dt);
 
-	static Image zeros(unsigned int width, unsigned int height);
-	static Image ones(unsigned int width, unsigned int height);
-
-	*/
 
 private:
-	int** m_data;
+	unsigned int** m_data;
 	unsigned int m_width;
 	unsigned int m_height;
 };
