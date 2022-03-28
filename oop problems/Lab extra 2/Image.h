b1e45359a18754546679a6adf13bcc5660203cca
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-03-28 20:38:32 +0300

    Update Image.h

--- oop problems/Lab extra 2/Image.h
+++ oop problems/Lab extra 2/Image.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <fstream>
 #include "Size.h"
+#include "Point.h"
+#include "Rectangle.h"
 
 class Image : public Size{
 public:
@@ -14,15 +16,39 @@ public:
 
 	unsigned int width() const;
 	unsigned int height() const;
-	
+
 	bool load(std::string imagePath);
 	bool save(std::string imagePath);
 
 	Size size() const;
 
-
 	friend std::ostream& operator<<(std::ostream& os, const Image& dt);
 
+	void operator=(const Image& other);
+
+	Image operator+(const Image& i);
+	Image operator-(const Image& i);
+	Image operator*(const Image& i);
+
+	Image operator+(const unsigned int scalar);
+	Image operator-(const unsigned int scalar);
+	Image operator*(const unsigned int scalar);
+
+	bool isEmpty() const;
+
+	unsigned int& at(unsigned int x, unsigned int y);
+	unsigned int& at(Point pt);
+
+	unsigned int* row(int y);
+
+	void release();
+	void add_img_to_scalar(unsigned int scalar);
+
+	Image getROI(unsigned int x, unsigned int y, unsigned int width, unsigned int height);
+	Image getROI(Rectangle roiRect);
+
+	static Image zeros(unsigned int width, unsigned int height);
+	static Image ones(unsigned int width, unsigned int height);
 
 private:
 	unsigned int** m_data;
