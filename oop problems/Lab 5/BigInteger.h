d61edc1923a8f5229281a52a002a1759351be812
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-03-31 10:05:47 +0300

    Update BigInteger.h

--- oop problems/Lab 5/BigInteger.h
+++ oop problems/Lab 5/BigInteger.h
@@ -1,84 +1,29 @@
-#include "BigInteger.h"
-
-#include <cctype> // for isdigit
-#include <iostream>
-#include <iomanip>
-using namespace std;
+#pragma once
+#include<string>
+#include<iostream>
 
 
-// addition -prepend cu 0 si adunam una cate una
-// apoi facem rez - 10 si facem carry
-// 16 - 10 = 6, carry 1
+class BigInteger {
 
-/*
-void digitize() {
-	int carry = 0;
-	for (i = num_digits - 1; i >= 0; i--) {
-		digits[i] += carry;
-		if (digits[i] > 9) {
-			carry = 1;
-			digits[i] -= 10;
-		}
-		else {
-			carry = 0;
-		}
-	}
-	if(carry) digits[0] = 1;
-}
-*/
+public:
+	BigInteger();
+	BigInteger(string* str);
 
-BigInteger::BigInteger() {
-	sign = 0;
-	num_digits = 0;
-	digits = new int[100]();
+	~BigInteger();
 
-}
+	int compare(const BigInteger N) const;
 
-BigInteger::BigInteger(string* str) {
-	if (str[0] == "-") {
-		sign = -1;
-	}
-	else if (str[0] == 0){
-		sign = 0;
-	}
-	else
-	{
-		sign = 1;
-	}
-	num_digits = length(str);
-	digits = new int[num_digits]();
-}
+	int BigInteger::operator==(const BigInteger N) const;
+	int BigInteger::operator<(const BigInteger N) const;
+	int BigInteger::operator>(const BigInteger N) const;
+	int BigInteger::operator<=(const BigInteger N) const;
+	int BigInteger::operator>=(const BigInteger N) const;
 
-BigInteger::~BigInteger(BigInteger* Digits) {
-	delete[] Digits;
-}
+	//BigInteger add(const BigInteger& N) const;
+	//BigInteger sub(const BigInteger& N) const;
 
-BigInteger::copy_constructor(const BigInteger& c) {
-	digits = c.digits;
-	*num_digits = *(c.num_digits);
-	sign = c.sign;
-}
-
-BigInteger::assignment_operator(const BigInteger& c) {
-	// release si apoi deep copy
-	digits = c.digits;
-	*num_digits = *(c.num_digits);
-	sign = c.sign;
-	
-}
-
-
-int compare(BigInteger N) {
-	// -1 if number in object is smaller than N, 0 if equal and 1 otherwise
-
-	if (this->sign > N.sign) {
-		return 1;
-	}
-	else if (this->sign < N.sign)
-		return -1;
-	
-	if (this->num_digits > N.num_digits) {
-
-	}
-	
-}
+private:
+	int* digits;
+	int num_digits;
+	int sign; // 1 -> >0 ; 0 == 0; -1 -> <0
+};
