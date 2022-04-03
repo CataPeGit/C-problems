3b865558e5464058deac52c0060880d27d6f2a99
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-04-03 22:59:28 +0300

    Update Convolution.h

--- oop problems/Lab extra 2/Convolution.h
+++ oop problems/Lab extra 2/Convolution.h
@@ -3,44 +3,56 @@
 
 class Convolution : public ImageProcessing {
 public:
-    
+
 
     Convolution() {
         m_range = 256;
-    }
+    };
 
     Convolution(unsigned int range) {
         m_range = range;
-    }
+    };
 
-    void Convolution::process(const Image& src, Image& dst)  {
+    void Convolution::process(const Image& src, Image& dst) {
 
-        for (unsigned int i = 1; i < src.height() - 1; i++) {
-            for (unsigned int j = 1; j < src.width() - 1; j++) {
-                if (i == 0 || j == 0 || j == src.height() || j == src.width() || i == src.height() || i == src.width()) {
-                    dst.at(i, j) = src.at_const(i, j);
-                    if (i == 511) {
-                       std::cout << i << " ";
-                    }
-                    continue;
-                }
-                else {
-                    unsigned int new_pixel = 0;
-                    unsigned int current_pixel_source = src.at_const(i, j);
 
-                    for (int y = -1; y <= 1; y++) {
-                        for (int x = -1; x <= 1; x++) {
-                            unsigned int current_kernel_value = src.at_const(i + y, j + x);
-                            new_pixel += (current_pixel_source * current_kernel_value);     
+ 
+        unsigned int current_kernel_value = 0;
+        unsigned int margin = 0;
+
+        for (unsigned int i = 0; i < src.height(); i++) {
+            for (unsigned int j = 0; j < src.width(); j++) {
+
+                if ((i == 0 || j == 0 || src.width() == j || src.height() == i))
+                    margin = 1;
+
+
+                unsigned int new_pixel = 0;
+                unsigned int origin = src.at_const(i, j);
+                for (int y = -1; y <= 1; y++) {
+                    for (int x = -1; x <= 1; x++) {
+
+                        if (margin == 1 && (x == -1 || y == -1)) {
+                            current_kernel_value = src.at_const(i, j);
+                            new_pixel = new_pixel + (origin * current_kernel_value);
+                        }
+                        else {
+                            current_kernel_value = src.at_const(i + y, j + x);
+                            new_pixel = new_pixel + (origin * current_kernel_value);
                         }
                     }
-                    new_pixel = new_pixel % m_range;
-                    dst.at(i, j) = new_pixel;
                 }
+                   
+                new_pixel = new_pixel % m_range;
+                std::cout << new_pixel << "  ";
+                dst.at(i, j) = new_pixel;
 
+                
+                //std::cout << dst.at(i, j) << "  ";
             }
         }
-    }
+    std::cout << dst;
+    };
 
 
 private:
