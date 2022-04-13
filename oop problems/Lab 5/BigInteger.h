efc31cb46465d872f7b59e43682f64873f2382dc
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-04-13 23:49:46 +0300

    Update BigInteger.h

--- oop problems/Lab 5/BigInteger.h
+++ oop problems/Lab 5/BigInteger.h
@@ -1,170 +1,41 @@
-#include "BigInteger.h"
+#pragma once
+#include<string>
+#include<iostream>
 
-#include <cctype> // for isdigit
-#include <iostream>
-#include <iomanip>
-using namespace std;
+using std::string;
 
+class BigInteger {
 
-// addition -prepend cu 0 si adunam una cate una
-// apoi facem rez - 10 si facem carry
-// 16 - 10 = 6, carry 1
+public:
+	BigInteger();
+	BigInteger(string str);
 
-/*
-void digitize() {
-	int carry = 0;
-	for (i = num_digits - 1; i >= 0; i--) {
-		digits[i] += carry;
-		if (digits[i] > 9) {
-			carry = 1;
-			digits[i] -= 10;
-		}
-		else {
-			carry = 0;
-		}
-	}
-	if(carry) digits[0] = 1;
-}
-*/
+	~BigInteger();
 
-BigInteger::BigInteger() {
-	sign = 0;
-	num_digits = 0;
-	digits = new int[100]();
+	BigInteger& operator=(const BigInteger N);
 
-}
+	int compare(const BigInteger N) const;
 
-/*
-BigInteger::BigInteger(string* str) {
-	if (str[0] == "-") {
-		sign = -1;
-	}
-	else if (str[0] == '0'){
-		sign = 0;
-	}
-	else
-	{
-		sign = 1;
-	}
-	num_digits = length(str);
-	digits = new int[num_digits]();
-	
-	for (int i = 0; i < num_digits; i++) {
-		digits[i] = (int)str[i];
-	}
-}
-*/
+	int get_num_digits() const;
+	int get_sign() const;
+	int get_digit(int pos) const;
+
+	friend bool operator==(const BigInteger& A, const BigInteger& B) const;
+	/*
+	friend bool operator<(const BigInteger N) const;
+	friend bool operator>(const BigInteger N) const;
+	friend bool operator<=(const BigInteger N) const;
+	friend bool operator>=(const BigInteger N) const;	
+	*/
 
 
-BigInteger::~BigInteger() {
-	delete[] digits;
-}
+	char* prepend_zeros(char* arr, int arr_sz, int new_sz);
 
-int BigInteger::compare(const BigInteger N) const{
-	// -1 if number in object is smaller than N, 0 if equal and 1 otherwise
+	//BigInteger add(const BigInteger& N) const;
+	//BigInteger sub(const BigInteger& N) const;
 
-	if (this->sign > N.sign) {
-		return 1;
-	}
-	else if (this->sign < N.sign)
-		return -1;
-	
-	if (this->num_digits > N.num_digits) {
-		return 1;
-	}
-
-	if (this->num_digits < N.num_digits) {
-		return 0;
-	}
-	
-	if (sign == -1) {
-		for (int i = 1; i < num_digits; i++) {
-			if (digits[i] < N.digits[i]) {
-				return 1;
-			}
-			else if (digits[i] > N.digits[i]) {
-				return -1;
-			}
-		}
-	}
-	
-	for (int i = 0; i < num_digits; i++) {
-		if (digits[i] > N.digits[i]) {
-			return 1;
-		}
-		else if (digits[i] < N.digits[i]) {
-			return -1;
-		}
-	}
-	
-	return 0;
-}
-
-int BigInteger::operator==(const BigInteger N) const {
-	return compare(N);
-}
-
-int BigInteger::operator<(const BigInteger N) const {
-	return compare(N);
-}
-
-int BigInteger::operator>(const BigInteger N) const {
-	return compare(N);
-}
-
-int BigInteger::operator<=(const BigInteger N) const {
-	return compare(N);
-}
-
-int BigInteger::operator>=(const BigInteger N) const {
-	return compare(N);
-}
-
-char* BigInteger::prepend_zeros(char* arr, int arr_sz, int new_sz) {
-	int gap_to_prepend = new_sz - arr_sz;
-	char* digits_new = new char[new_sz]();
-	int digit = 0;
-	for (int i = 0; i < new_sz; i++) {
-		if (i >= gap_to_prepend) {
-			arr[i] = digits[digit];
-		}
-		arr[i] = 0;
-	}
-	return arr;
-}
-
-
-
-
-/*
-BigInteger BigInteger::add(const BigInteger& N) const {
-	if (this->sign == N.sign) {
-		if (this->sign == "-") {
-			for (int i = num; i++) {
-				this->num_digits[i] += num_digits[i];
-			}
-		}
-		else {
-			for (int i = 0; i < num_digits; i++) {
-				this->num_digits[i] += num_digits[i];
-			}
-		}
-	}
-
-}
-
-BigInteger::copy_constructor(const BigInteger& c) {
-	digits = c.digits;
-	*num_digits = *(c.num_digits);
-	sign = c.sign;
-}
-
-BigInteger::assignment_operator(const BigInteger& c) {
-	// release si apoi deep copy
-	digits = c.digits;
-	*num_digits = *(c.num_digits);
-	sign = c.sign;
-
-}
-
-*/
+private:
+	int* digits;
+	int num_digits;
+	int sign; // 1 -> >0 ; 0 == 0; -1 -> <0
+};
