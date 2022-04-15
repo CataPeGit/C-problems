bebe1b06fbd85fc07616a1c522500f4147adea74
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-04-15 18:14:24 +0300

    Update BigInteger.h

--- oop problems/Lab 5/BigInteger.h
+++ oop problems/Lab 5/BigInteger.h
@@ -3,12 +3,14 @@
 #include<iostream>
 
 using std::string;
+using std::max;
 
 class BigInteger {
 
 public:
 	BigInteger();
 	BigInteger(string str);
+	BigInteger(int* _digits, int _num_digits, int _sign);
 
 	~BigInteger();
 
@@ -18,24 +20,37 @@ public:
 
 	int get_num_digits() const;
 	int get_sign() const;
+	int* get_digits_array() const;
 	int get_digit(int pos) const;
 
-	friend bool operator==(const BigInteger& A, const BigInteger& B) const;
+	friend bool operator==(const BigInteger& A, const BigInteger& B);
+	friend bool operator<(const BigInteger& A, const BigInteger& B);
+	friend bool operator>(const BigInteger& A, const BigInteger& B);
+	friend bool operator<=(const BigInteger& A, const BigInteger& B);
+	friend bool operator>=(const BigInteger& A, const BigInteger& B);
+
+	friend BigInteger operator+(const BigInteger& A, const BigInteger& B);
+	friend BigInteger operator-(const BigInteger& A, const BigInteger& B);
+
+	//BigInteger sub(const BigInteger& N) const;
 	/*
-	friend bool operator<(const BigInteger N) const;
-	friend bool operator>(const BigInteger N) const;
-	friend bool operator<=(const BigInteger N) const;
-	friend bool operator>=(const BigInteger N) const;	
+	int* prepend_zeros(int* arr, int arr_sz, int new_sz);
+	int* negate(int* arr, int arr_sz);
+	int* add(int* arr1, int* arr2, int arr_sz);
+	int* digitize(int* arr, int arr_sz);	
 	*/
 
-
-	char* prepend_zeros(char* arr, int arr_sz, int new_sz);
-
-	//BigInteger add(const BigInteger& N) const;
+	BigInteger add(const BigInteger& N) const;
 	//BigInteger sub(const BigInteger& N) const;
 
 private:
 	int* digits;
 	int num_digits;
 	int sign; // 1 -> >0 ; 0 == 0; -1 -> <0
+
+
+	
+
+	//BigInteger sub(const BigInteger& N) const;
+
 };
