780e023db8f0625a70633a6b90d3b6cad7bf5fbb
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-04-29 18:57:18 +0300

    Update User.cpp

--- oop problems/more code/User.cpp
+++ oop problems/more code/User.cpp
@@ -1,5 +1,29 @@
 #include "User.h"
 
+User::User() {
+	id = NULL;
+	name = "";
+	account_balance = 0;
+	spent_money_on[""] = 0;
+}
+User::User(const User& other_user) {
+	id = other_user.get_id();
+	name = other_user.get_name();
+	account_balance = other_user.get_account_balance();
+	spent_money_on = other_user.get_spent_money_on();
+}
+User::~User() {
+
+}
+
+User::User(unsigned int _id, string _name, unsigned int _account_balance, std::map<std::string, unsigned int> _spent_money_on) {
+	id = _id;
+	name = _name;
+	account_balance = _account_balance;
+	spent_money_on = _spent_money_on;
+}
+
+// getters
 unsigned int User::get_id() const {
 	return id;
 }
@@ -11,6 +35,11 @@ unsigned int User::get_account_balance() const {
 	return account_balance;
 }
 
+std::map<std::string, unsigned int> User::get_spent_money_on() const {
+	return spent_money_on;
+}
+
+//setters
 void User::add_to_balance(unsigned int value_to_add)
 {
 	account_balance += value_to_add;
