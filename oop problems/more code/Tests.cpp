fbcc461674625de28165725625744adde10fc409
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-04-29 21:36:45 +0300

    added tests

---
+++ oop problems/more code/Tests.cpp
@@ -0,0 +1,63 @@
+#include "Tests.h"
+
+
+void Tests::runAllTests()
+{
+    test_user();
+    test_null_user();
+}
+
+void Tests::test_user()
+{
+    std::map<std::string, unsigned int> themap;
+
+    themap["water"] = 520;
+
+    User testedUser(0, "Test Name", 1000, themap);
+
+    assert(testedUser.get_id() == 0);
+    assert(testedUser.get_name() == "Test Name");
+    assert(testedUser.get_account_balance() == 1000);
+
+    std::map<std::string, unsigned int> check_map = testedUser.get_purchased_price();
+    assert(themap.size() == check_map.size() && std::equal(themap.begin(), themap.end(), check_map.begin()));
+
+    testedUser.add_purchase("water", 30);
+    assert(testedUser.get_specific_purchase_price("water") == 550);
+    testedUser.add_purchase("food", 400);
+    assert(testedUser.get_specific_purchase_price("food") == 400);
+
+    testedUser.add_to_balance(500);
+    assert(testedUser.get_account_balance() == 1500);
+    testedUser.substract_from_balance(500);
+    assert(testedUser.get_account_balance() == 1000);
+    testedUser.set_balance(1500);
+    assert(testedUser.get_account_balance() == 1500);
+
+    testedUser.set_specific_purchase_price("food", 750);
+    assert(testedUser.get_specific_purchase_price("food") == 750);
+    assert(testedUser.get_specific_purchase_price("food") != 400);
+
+    testedUser.remove_purchase("food");
+    assert(testedUser.check_if_purchase_exists("food") == false);
+    try {
+        testedUser.remove_purchase("catch me");
+        assert(false);
+    }
+    catch (int catch_int) {
+        assert(catch_int == 404);
+    }
+    assert(testedUser.get_total_balance_spent() == 550);
+}
+
+void Tests::test_null_user() {
+
+    User test_null_user;
+    assert(test_null_user.get_id() == NULL);
+    assert(test_null_user.get_name() == "");
+    assert(test_null_user.get_account_balance() == 0);
+
+    std::map<std::string, unsigned int> check_map = test_null_user.get_purchased_price();
+    assert(check_map[""] == NULL);
+
+}
---
+++ oop problems/more code/Tests.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "User.h"
+#include <cassert>
+class Tests {
+
+public:
+	static void runAllTests();
+	static void test_user();
+	static void test_null_user();
+};
