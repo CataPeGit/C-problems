539cee2a33e5a18f48b954d68389b876e30a9ce9
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-05-09 23:46:05 +0300

    Update Repository.cpp

--- oop problems/more code/Repository.cpp
+++ oop problems/more code/Repository.cpp
@@ -15,7 +15,7 @@ std::vector<User> Repository::get_rep() const
 	return user_repository;
 }
 
-unsigned int Repository::get_latest_user_id() const
+unsigned int Repository::get_size() const
 {
 	return user_repository.size();
 }
@@ -40,4 +40,70 @@ void Repository::remove_user_from_repository(User user){
 
 void Repository::clear_repository(){
 	user_repository.clear();
+}
+
+vector<string> tokenize(string str, char delimiter)
+{
+	// tokenize a given string based on a delimiter
+	// usage example: tokanize types of user data in order to save from/to repository 
+	vector<string> result;
+	stringstream ss(str);
+	string token;
+	while (getline(ss, token, delimiter)) {
+		result.push_back(token);
+	}
+	return result;
+}
+
+
+
+
+void Repository::read_from_file()
+{
+	// reading the data file and saving all user information into repositoy
+	ifstream data_file;
+	data_file.open("data.csv");
+
+	string line_information;
+	while (getline(data_file, line_information)) {
+		// Output the text from the file
+		vector<string> data = tokenize(line_information, ',');
+		User new_user(std::stoi(data[0]), data[1], std::stoi(data[2]));
+		
+		// we need to treat the purchases map separately
+		// one purchase will be represented by an item name and price, one next to the other
+		int i = 3;
+		while (i < data.size()) {
+			new_user.add_purchase_to_map(data[i], std::stoi(data[i+1]));
+			i += 2;
+		}
+		
+		user_repository.push_back(new_user);
+		
+		line_information.clear();
+	}
+	data_file.close();
+}
+
+void Repository::write_to_file() {
+	// writing data from the repository to the data file
+	ofstream data_file;
+	data_file.open("data.csv");
+
+	for (User current_user : user_repository) {
+		data_file << current_user.get_id() << "," << current_user.get_name() << "," << current_user.get_account_balance();
+		std::map<std::string, unsigned int> current_user_purchases = current_user.get_purchased_price();
+		
+		map<string, unsigned int>::iterator it;
+		for (it = current_user_purchases.begin(); it != current_user_purchases.end(); it++)
+		{
+			if (it == current_user_purchases.begin())
+				data_file << ",";
+			data_file << it->first << "," << it->second << ",";
+		}
+
+		data_file << "\n";
+	}
+
+	data_file.close();
 }
