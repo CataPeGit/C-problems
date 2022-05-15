f0e320d6ce415330769cd4b2c258d286e107aaa7
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2022-05-15 23:11:15 +0300

    Update Puzzle.h

--- puzzle/Puzzle.h
+++ puzzle/Puzzle.h
@@ -2,14 +2,21 @@
 #include "Graph.h"
 #include <string>
 #include <ostream>
-#include <iomanip>
+#include <iostream>
 #include <fstream>
+#include <queue>
+#include <utility>
 
+using std::pair;
+using std::queue;
 using std::string;
 using std::ifstream;
 using std::getline;
 
-class Puzzle {
+#include<list>
+using std::list;
+
+class Puzzle : public Graph<string> {
 
 public:
 
@@ -20,8 +27,24 @@ public:
 	// getting the current graph
 	Graph<string> get_graph() const;
 
-	// getting all words of a given length from the file containg all words
+	// getting all words of a given length from the file containg all words into a map
 	map<string, vector<string>> get_all_words(string filePath, int length);
+
+	// adding all words from "get_all_words" to the current "graph" object
+	void fill_graph_with_all_words(string filePath, int length);
+
+	// a pattern is defined by a word in which one and only one of its letters have been replaced by a *
+	// and therefore it can be replaced by any letter
+	// this function creates all possible pattern for a given word
+	// and adds them to the map given as a parameter
+	void create_possible_patterns(map<string, vector<string>>& word_map, string word);
+
+	// creating a graph containing all words from a map
+	Graph<string> create_graph_from_map(map<string, vector<string>> word_map);
+
+	vector<string> bfs_path(string start_node, string end_node);
+
+
 
 private:
 	
@@ -30,12 +53,3 @@ private:
 	Graph<string> graph;
 
 };
-
-
-
-
-
-
-
-
-
